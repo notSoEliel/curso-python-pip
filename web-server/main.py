386790e0fd6a926e8289afdd47e9c5bf559f292e
Eliel García <bgt.eliel@gmail.com>
2024-09-02 15:52:28 -0500

    Uvicorn + FastAPI implementation

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,21 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI() #Instancia de FastAPI
+
+@app.get('/list') #Funcion decorada: Cada que se obtenga la ruta '/list' se ejecutara la siguiente funcion
+def get_list():
+    return [2,3,4,5,6,7,8,9,0] #Devolvera una lista de nuemros 
+
+@app.get('/', response_class=HTMLResponse) #Funcion decorada: Cada que se obtenga la ruta '/' tendra una respuesta de tipo HTMl
+def get_main():
+    #Esto es para que pueda soportar una solicitud HTML y funcione la pagina
+    return """    
+        <h2>Helloo</h2>
+        <p>parapara</p>
+        <!-- img src="quico.jpg" width="300" height="200"> -->
+    """
 
 def run():
     store.get_categories()
